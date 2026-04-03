6d9f78fb596601b6569ccb3430bf4e9c0d040a15
Vasetti Janakirao <janakirao4701@gmail.com>
2026-04-03 06:05:05 +0530

    chore: auto-deploy updates to live site

--- sw.js
+++ sw.js
@@ -1,19 +1,31 @@
 // sw.js - Basic Service Worker to satisfy PWA requirements
-const CACHE_NAME = 'job-tracker-v1';
+const CACHE_NAME = 'job-tracker-v2';
 const ASSETS = [
   '/',
   '/index.html',
   '/lib/config.js',
   '/lib/xlsxbuilder.js',
   '/scripts/app.js',
-  '/styles/sidebar.css',
+  '/icons/icon48.png',
+  '/icons/icon128.png',
   '/icons/icon192.png',
   '/icons/icon512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
 
